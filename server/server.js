8aee58f0663e9933d05af41edb61980fe6d0c788
IanGlass <37228869+IanGlass@users.noreply.github.com>
2019-09-09 08:12:07 +1200

    Move public assets to /public/dist/

--- server/server.js
+++ server/server.js
@@ -6,7 +6,6 @@ const publicPath = path.join(__dirname, '..', 'public');
 app.use(express.static(publicPath));
 
 app.get('*', (req, res) => {
-  // res.redirect('/');
   res.sendFile(path.join(publicPath, 'index.html'));
 });
 
