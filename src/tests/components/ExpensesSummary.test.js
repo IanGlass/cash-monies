0bdbc09f981e4d4f1f23018d13a8070bf090ee9f
IanGlass <37228869+IanGlass@users.noreply.github.com>
2019-09-10 18:54:22 +1200

    Add snapshot testing for ExpensesSummary

--- src/tests/components/ExpensesSummary.test.js
+++ src/tests/components/ExpensesSummary.test.js
@@ -11,6 +11,6 @@ test('should render ExpensesSummary with no expenses', () => {
 });
 
 test('should render ExpensesSummary with expenses summary', () => {
-  const wrapper = shallow(<ExpensesSummary expensesCount={2} expensesTotal={numeral(50199 / 100).format('$0,0.00')} />);
-  console.log(wrapper.debug());
+  const wrapper = shallow(<ExpensesSummary expensesCount={2} expensesTotal={50199} />);
+  expect(wrapper).toMatchSnapshot();
 })
