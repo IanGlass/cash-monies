c725799da59e4f073f48f2296fc2a3c03205bcf8
IanGlass <37228869+IanGlass@users.noreply.github.com>
2019-09-17 20:32:41 +1200

    Redirect user for login and logout

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 import { firebase } from './firebase/firebase';
 import { startSetExpenses } from './actions/expenses';
+import { history } from './routers/AppRouter';
 
 const store = configureStore();
 
@@ -17,18 +18,29 @@ const jsx = (
   </Provider>
 );
 
+let hasRendered = false;
+const renderApp = () => {
+  if (!hasRendered) {
+    ReactDOM.render(jsx, document.getElementById('app'));
+    hasRendered = true;
+  }
+}
+
 ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 
-// Wait until expenses are loaded from DB before rendering application
-store.dispatch(startSetExpenses())
-  .then(() => {
-    ReactDOM.render(jsx, document.getElementById('app'));
-  });
-
 firebase.auth().onAuthStateChanged((user) => {
-  if (user) {
-    console.log('login');
-  } else {
-    console.log('logout');
+  // Log user out
+  if (!user) {
+    renderApp();
+    return history.push('/');
   }
-})
+  // Log user in
+  // Wait until expenses are loaded from DB before rendering application
+  store.dispatch(startSetExpenses())
+  .then(() => {
+    renderApp();
+    if (history.location.pathname === '/') {
+      return history.push('/dashboard');
+    }
+  });
+});
