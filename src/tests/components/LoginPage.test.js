8a6e155b5c9bb59a36291195342aa504975bb871
IanGlass <37228869+IanGlass@users.noreply.github.com>
2019-09-17 18:28:37 +1200

    Add testing for LoginPage login button

--- src/tests/components/LoginPage.test.js
+++ src/tests/components/LoginPage.test.js
@@ -6,4 +6,12 @@ import { LoginPage } from '../../components/LoginPage';
 test('should render LoginPage', () => {
   const wrapper = shallow(<LoginPage />);
   expect(wrapper).toMatchSnapshot();
-})
+});
+
+test('should call startLogin on button click', () => {
+  const startLogin = jest.fn();
+  const wrapper = shallow(<LoginPage startLogin={startLogin} />);
+  wrapper.find('button').simulate('click');
+  expect(startLogin).toHaveBeenCalled();
+  expect(wrapper).toMatchSnapshot();
+});
