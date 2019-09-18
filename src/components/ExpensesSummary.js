3f22a9010a4a532f1e2c27cef3e1870cad2921b8
IanGlass <37228869+IanGlass@users.noreply.github.com>
2019-09-19 07:36:21 +1200

    Style the logout button and Add Expense button

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import numeral from 'numeral';
 
 import expensesTotal from '../selectors/expenses-total';
@@ -9,6 +10,9 @@ export const ExpensesSummary = (props) => (
   <div className="page-header">
     <div className="content-container">
       <h1 className="page-header__title">Viewing <span>{props.expensesCount}</span> {props.expensesCount === 1 ? 'expense' : 'expenses'} with total <span>{numeral(props.expensesTotal / 100).format('$0,0.00')}</span></h1>
+      <div className="page-header__actions">
+        <Link to="/create" className="button">Add Expense</Link>
+      </div>
     </div>
   </div>
 );
