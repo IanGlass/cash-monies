d657fe3cea6741b2c9f23393538d2d33cd4dfe2c
IanGlass <37228869+IanGlass@users.noreply.github.com>
2019-09-21 15:03:44 +1200

    Add loader when loading app

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ import { firebase } from './firebase/firebase';
 import { startSetExpenses } from './actions/expenses';
 import { history } from './routers/AppRouter';
 import { login, logout } from './actions/auth';
+import LoadingPage from './components/LoadingPage';
 
 const store = configureStore();
 
@@ -27,7 +28,7 @@ const renderApp = () => {
   }
 }
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+ReactDOM.render(<LoadingPage />, document.getElementById('app'));
 
 firebase.auth().onAuthStateChanged((user) => {
   // Log user out
