35f4f07466cdbd54edcf881c3aacb9d962f1dda2
IanGlass <37228869+IanGlass@users.noreply.github.com>
2019-09-21 21:01:17 +1200

    Add confirmation modal for removing expense, closes #2

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,16 +1,36 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import Modal from 'react-modal';
+
 import ExpenseForm from './ExpenseForm';
 import { startEditExpense, startRemoveExpense } from '../actions/expenses';
 
 export class EditExpensePage extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      showModal: false,
+    }
+  }
   onSubmit = (expense) => {
     this.props.startEditExpense(this.props.expense.id, expense);
     this.props.history.push('/');
   }
-  onClick = () => {
+  removeExpense = () => {
+    this.closeModal();
     this.props.startRemoveExpense(this.props.expense.id);
     this.props.history.push('/');
+  }
+  openModal = () => {
+    this.setState(() => ({
+      showModal: true
+    }));
+  }
+  closeModal = () => {
+    this.setState(() => ({
+      showModal: false
+    }));
   }
   render() {
     return (
@@ -26,8 +46,21 @@ export class EditExpensePage extends React.Component {
             onSubmit={this.onSubmit}
           />
           <button className="button button--secondary"
-            onClick={this.onClick}
+            onClick={this.openModal}
           >Remove Expense</button>
+          <Modal
+            isOpen={this.state.showModal}
+            onRequestClose={this.closeModal}
+            ariaHideApp={false}
+            className="modal"
+            contentLabel="Confirm Remove Expense"
+          >
+            <p>Are you sure?</p>
+            <div className="button--group">
+              <button className="button" onClick={this.removeExpense}>Yes</button>
+              <button className="button" onClick={this.closeModal}>No</button>
+            </div>
+          </Modal>
         </div>
       </div>
     );
